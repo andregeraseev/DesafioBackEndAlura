c0a5a6f9e17ee82bab23eb610806d4516a303b77
Andre Geraseev <ageraseev@gmail.com>
2022-08-08 08:53:09 -0300

    adicionando categorias e filtro por descricao nas despesas

--- api/renda/models.py
+++ api/renda/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Receita(models.Model):
 
     descricao = models.CharField(max_length=100, unique_for_month="data")
-    valor = models.IntegerField()
+    valor = models.DecimalField(max_digits=8, decimal_places=2)
     categoria = models.CharField(max_length=50)
     data = models.DateField()
 
@@ -11,10 +11,20 @@ class Receita(models.Model):
         return self.descricao
 
 class Despesas(models.Model):
+    CATEGORIAS = (
+        ('A', 'Alimentação'),
+        ('S', 'Saúde'),
+        ('M', 'Moradia'),
+        ('T', 'Transporte'),
+        ('E', 'Educação'),
+        ('L', 'Lazer'),
+        ('I', 'Imprevistos'),
+        ('O', 'Outras')
+    )
 
     descricao = models.CharField(unique_for_month="data", max_length=100)
-    valor = models.IntegerField()
-    categoria = models.CharField(max_length=50)
+    valor = models.DecimalField(max_digits=8, decimal_places=2)
+    categoria = models.CharField(max_length=1, null=False, blank=False, choices= CATEGORIAS, default='O')
     data = models.DateField()
 
     def __str__(self):
--- api/renda/views.py
+++ api/renda/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import BasicAuthentication
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Receita, Despesas
 from .serializers import ReceitaSerializer, DespesasSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class ReceitaViewSet(viewsets.ModelViewSet):
     queryset = Receita.objects.all()
@@ -15,5 +17,8 @@ class ReceitaViewSet(viewsets.ModelViewSet):
 class DespesasViewSet(viewsets.ModelViewSet):
     queryset = Despesas.objects.all()
     serializer_class = DespesasSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['descricao']
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
+
