d6854bb2ba93dee03b774ab898f6648b15badff4
Andre Geraseev <ageraseev@gmail.com>
2022-08-09 08:24:00 -0300

    filtro descricao url, filtro por ano/mes url

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework import routers
-from renda.views import ReceitaViewSet, DespesasViewSet
+from renda.views import ReceitaViewSet, DespesasViewSet, BuscaDespesasList,BuscaReceitasList,ListaReceitasMes
 
 router = routers.DefaultRouter()
-router.register(r'receita', ReceitaViewSet)
+router.register(r'receitas', ReceitaViewSet)
 router.register(r'despesas', DespesasViewSet)
 
 
@@ -12,5 +12,8 @@ urlpatterns = [
     path('', include((router.urls))),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    # path(r'despesas/', FiltroDespesaAPIView.as_view(), name='despesas-filtro')
+    path('despesas?descricao=<slug:pk>', BuscaDespesasList.as_view()),
+    path('receitas?descricao=<slug:pk>', BuscaReceitasList.as_view()),
+    path('receitas/<int:ano>/<int:mes>/', ListaReceitasMes.as_view()),
+
 ]
--- api/renda/views.py
+++ api/renda/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import BasicAuthentication
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, generics
 from .models import Receita, Despesas
 from .serializers import ReceitaSerializer, DespesasSerializer
 from django_filters.rest_framework import DjangoFilterBackend
@@ -16,6 +16,24 @@ class ReceitaViewSet(viewsets.ModelViewSet):
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
 
+class BuscaReceitasList(generics.ListAPIView):
+    """Busca receita pela descricao"""
+    queryset = Despesas.objects.all()
+    serializer_class = ReceitaSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filter_backends[1].search_param = 'descricao'
+    search_fields = ['descricao']
+
+class ListaReceitasMes(generics.ListAPIView):
+    """Exibindo todas as receitas de um determinado mês"""
+
+    def get_queryset(self):
+
+        queryset = Receita.objects.filter(data__year=self.kwargs['ano'], data__month=self.kwargs['mes'])
+        return queryset
+
+    serializer_class = ReceitaSerializer
+
 
 class DespesasViewSet(viewsets.ModelViewSet):
     queryset = Despesas.objects.all()
@@ -23,9 +41,18 @@ class DespesasViewSet(viewsets.ModelViewSet):
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     search_fields = ['descricao']
-    filter_fields = ['descricao']
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
+    filter_backends[1].search_param = 'descricao'
+    search_fields = ['descricao']
+
+class BuscaDespesasList(generics.ListAPIView):
+    """Busca despesas pela descricao"""
+    queryset = Despesas.objects.all()
+    serializer_class = DespesasSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filter_backends[1].search_param = 'descricao'
+    search_fields = ['descricao']
 
 
 
