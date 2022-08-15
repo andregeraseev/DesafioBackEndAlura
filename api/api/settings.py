f1fe6dd7bdb523ebe633a669e6b4cbda42c70b75
Andre Geraseev <ageraseev@gmail.com>
2022-08-15 11:35:20 -0300

    make it better

--- api/api/settings.py
+++ api/api/settings.py
@@ -131,8 +131,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'],
-    #     'rest_framework.permissions.DjangoModelPermissions'
-    # ],
+    # #     'rest_framework.permissions.DjangoModelPermissions'
+    # # ],
     'DEFAULT_AUTHENTICATION_CLASSES':[
         'rest_framework.authentication.BasicAuthentication'
     ],
